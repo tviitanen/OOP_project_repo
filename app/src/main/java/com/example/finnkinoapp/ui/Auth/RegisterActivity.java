98d66d8cb9df12bbb25f7a14898fab136468cbbc
Teemu Viitanen <teemu.viitanen@gmail.com>
2022-04-22 16:26:34 +0300

    added back button for action bar in register view

--- app/src/main/java/com/example/finnkinoapp/ui/Auth/RegisterActivity.java
+++ app/src/main/java/com/example/finnkinoapp/ui/Auth/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.example.finnkinoapp.ui.Auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.finnkinoapp.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -10,12 +13,26 @@ import com.google.firebase.auth.FirebaseAuth;
 public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
