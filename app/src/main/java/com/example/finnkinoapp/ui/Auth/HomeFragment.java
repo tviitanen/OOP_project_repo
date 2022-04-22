ed7950b730fd5041d428666acc26bb8679f0b656
Teemu Viitanen <teemu.viitanen@gmail.com>
2022-04-22 22:17:06 +0300

    created login activity and functionality to login using credentials

--- app/src/main/java/com/example/finnkinoapp/ui/Auth/HomeFragment.java
+++ app/src/main/java/com/example/finnkinoapp/ui/Auth/HomeFragment.java
@@ -2,24 +2,36 @@ package com.example.finnkinoapp.ui.Auth;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.finnkinoapp.MainActivity;
 import com.example.finnkinoapp.R;
 import com.example.finnkinoapp.databinding.FragmentHomeBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeFragment extends Fragment implements  View.OnClickListener {
 
     private FragmentHomeBinding binding;
     private TextView register;
-
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button logIn;
+    private FirebaseAuth mAuth;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
@@ -27,6 +39,14 @@ public class HomeFragment extends Fragment implements  View.OnClickListener {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // Login button
+        logIn = binding.loginButton;
+        logIn.setOnClickListener(this);
+
+        // user inputs email & password
+        editTextEmail = binding.editTextTextEmailAddress;
+        editTextPassword = binding.editTextTextPassword;
 
         // register button
         register = binding.registerUser;
@@ -50,6 +70,52 @@ public class HomeFragment extends Fragment implements  View.OnClickListener {
             case R.id.registerUser:
                 startActivity(new Intent(getActivity().getBaseContext(), RegisterActivity.class));
                 break;
+            case R.id.login_button:
+                userLogin();
+                break;
         }
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required!");
+            editTextEmail.requestFocus();
+            return;
+        }
+        // check email format
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Email is incorrect!");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required!");
+            editTextPassword.requestFocus();
+            return;
+        }
+        // check lenght of the password  (firebase minimum lenght is 6)
+        if (password.length() < 6) {
+            editTextPassword.setError("Password too short. Atleast 6 characters required.");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        // firebase authorization
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                // if data added to a db succesfully, make a toast notice
+                if (task.isSuccessful()) {
+                    // redirect to user profile
+                    Toast.makeText(getActivity(), "\"Login completed successfully!\"", Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(getActivity(), ProfileActivity.class));
+                } else {
+                Toast.makeText(getActivity(), "\"An error occurred during login, check your credentials!\"", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/finnkinoapp/ui/Auth/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.finnkinoapp.ui.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finnkinoapp.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.finnkinoapp.R.layout.activity_profile);
+    }
+}
