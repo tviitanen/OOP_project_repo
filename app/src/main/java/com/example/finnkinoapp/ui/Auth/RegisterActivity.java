dce99c8ac9870d497cf3bac7fc48c9f9ab6c6d87
Teemu Viitanen <teemu.viitanen@gmail.com>
2022-04-22 14:03:04 +0300

    layout for register activity

---
+++ app/src/main/java/com/example/finnkinoapp/ui/Auth/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.finnkinoapp.ui.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finnkinoapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
